3a921762a5ad6f1c8ecbe1277ec1b1aa65773ce1
luk3mn <lucasrenan2015@gmail.com>
2022-08-14 11:25:53 -0300

    Ajustando o front-end e criando novas rotas no back-end

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='plataforma.home'),
     path('autenticacao/', views.autenticacao, name='plataforma.autenticacao'),
     path('sobre/', views.sobre, name='plataforma.sobre'),
+    path('curso/<int:id_curso>/', views.curso, name='plataforma.curso'),
+    path('curso/disciplinas/', views.disciplinas, name='plataforma.disciplinas'),
 ]
--- plataforma/views.py
+++ plataforma/views.py
@@ -12,6 +12,12 @@ def autenticacao(request):
 def sobre(request):
     return render(request, 'plataforma/sobre.html')
 
-# def listar_cursos(request):
-#     curso = Curso.objects.all()
-#     return render(request, 'plataforma/home.html', {'cursos':curso})
+def curso(request, id_curso):
+    # curso = Curso.objects.filter(id=id_curso)
+    curso = Curso.objects.get(id=id_curso)
+    return render(request, 'plataforma/curso.html', {'curso':curso})
+    # return HttpResponse(curso.descricao)
+
+def disciplinas(request):
+    # curso = Curso.objects.all()
+    return render(request, 'plataforma/lista_disciplinas.html')
