be702ecda293fdb0892493a7333b2ddcd97f7cc0
luk3mn <lucasrenan2015@gmail.com>
2022-08-15 11:10:58 -0300

    ajustando o front-end e adicionando novas rotas como alunos

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('autenticacao/', views.autenticacao, name='plataforma.autenticacao'),
     path('sobre/', views.sobre, name='plataforma.sobre'),
     path('curso/<int:id_curso>/', views.curso, name='plataforma.curso'),
-    path('curso/disciplinas/', views.disciplinas, name='plataforma.disciplinas'),
+    path('curso/disciplinas/<int:id_curso>/', views.disciplinas, name='plataforma.disciplinas'),
+    path('curso/alunos/', views.lista_alunos, name='plataforma.alunos'),
 ]
--- plataforma/views.py
+++ plataforma/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from plataforma.models import Curso
+from plataforma.models import Curso, Disciplina
+from datetime import datetime
 
+# agora = datetime.today().strftime('%Y')
 def home(request):
     curso = Curso.objects.all()
     return render(request, 'plataforma/home.html', {'cursos':curso})
@@ -16,8 +18,11 @@ def curso(request, id_curso):
     # curso = Curso.objects.filter(id=id_curso)
     curso = Curso.objects.get(id=id_curso)
     return render(request, 'plataforma/curso.html', {'curso':curso})
-    # return HttpResponse(curso.descricao)
 
-def disciplinas(request):
-    # curso = Curso.objects.all()
-    return render(request, 'plataforma/lista_disciplinas.html')
+def disciplinas(request, id_curso):
+    disciplinas = Disciplina.objects.filter(curso_id=id_curso)
+    # return HttpResponse(agora)
+    return render(request, 'plataforma/lista_disciplinas.html', {'disciplinas':disciplinas})
+
+def lista_alunos(request):
+    return render(request, 'plataforma/lista_alunos.html')
